cb755bd7c977e018ac3703363946117e46336da9
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-18 21:03:23 +0100

    new changes

---
+++ src/context.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useContext } from 'react'
+
+const AppContext = createContext()
+
+export const AppProvider = ({children}) => {
+
+  return (
+   <AppContext.Provider value={{
+
+   }}>
+            {children}
+   </AppContext.Provider>
+  )
+}
+
+export const UseGlobalContext = ()=>{
+    return useContext(AppContext)
+}
+
+
