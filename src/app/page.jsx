5f7e2c3f0f295c7f7dc8749b6fa85111f0272762
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-18 20:57:06 +0100

    new changes

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+//https://cdn.prod.website-files.com/637359c81e22b715cec245ad/6627414178f226ce86b98804_right.svg
+//https://cdn.prod.website-files.com/637359c81e22b715cec245ad/662741410889f2489acd78d8_left.svg
+//https://polytech-assets.polygon.technology/videos/homepage/newsletter.mp4
+//https://cdn.prod.website-files.com/637359c81e22b715cec245ad/6464a7ec8c8fd22869e80364_home-hero-new-bg1.svg
+//https://cdn.prod.website-files.com/637359c81e22b715cec245ad/6486fd415a318e62764b3a6c_arrow-right.svg
+
+
+//videos_Carousel
+//https://polytech-assets.polygon.technology/videos/unstoppable_domains_wide.mp4
+//https://polytech-assets.polygon.technology/videos/Lemon_wide.mp4
+//https://polytech-assets.polygon.technology/videos/Aragon_wide.mp4
+//https://polytech-assets.polygon.technology/videos/horizon_wide1.mp4
+//https://polytech-assets.polygon.technology/videos/phantom_wallet_wide.mp4
+//https://polytech-assets.polygon.technology/videos/intella_x_wide1.mp4
+
+
+export default function Home() {
+  return (
+    <div className=""></div>
+  );
+}
