d2eca4d34531205710bc5da4dfe92e481dacfdce
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-21 21:22:16 +0100

    new changes

--- src/app/grant/page.jsx
+++ src/app/grant/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const page = () => {
+const Grant = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Grant
