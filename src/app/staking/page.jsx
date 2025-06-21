9e2a92308108853607e9c25d27a399c490ae503a
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-21 21:33:24 +0100

    new changes

--- src/app/staking/page.jsx
+++ src/app/staking/page.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Stake = () => {
   return (
    <section className="">
-   
+   <div className="">
+    
+    <h2>
+      <video src=""></video>
+      <div className="">
+        <h2></h2>
+      </div>
+    </h2>
+   </div>
 
    </section>
   )
