8f6e9ef1a3e7b5fec005f7c12afe6220681ad137
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-21 21:39:22 +0100

    new changes

--- src/app/staking/page.jsx
+++ src/app/staking/page.jsx
@@ -8,7 +8,9 @@ const Stake = () => {
     <h2>
       <video src=""></video>
       <div className="">
-        <h2></h2>
+        <h2> Earn rewards while securing the Polygon PoS network</h2>
+        <p>Anyone can use POL tokens to help safeguard the network and earn rewards</p>
+        <button className="">Becoma A Delegator</button>
       </div>
     </h2>
    </div>
