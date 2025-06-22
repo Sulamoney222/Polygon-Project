e9c5b9368c6f63881c5315f7074538524e1f55d8
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-22 23:27:01 +0100

    new changes

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -15,7 +15,7 @@ const Blog = () => {
           <source src="https://polytech-assets.polygon.technology/videos/plonky/hero_plonky-new.mp4" type="" />
         </video>
         <h1>Polygon Plonky3 enables you to build modern zk-rollups</h1>
-        <p>Lightning fast and modular, Polygon Plonky3 is a build-your-own adventure of ZK proving systems. Build new zkVMs or zkEVMs that crackle with a prover customized for specific use cases</p>
+        <p>Lightning ffff fast and modular, Polygon Plonky3 is a build-your-own adventure of ZK proving systems. Build new zkVMs or zkEVMs that crackle with a prover customized for specific use cases</p>
       </div>
     </section>
   )
