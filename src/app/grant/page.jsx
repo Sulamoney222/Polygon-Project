4bb2a2aa3b4c7baedddb33106c7601155f16609e
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-25 22:28:51 +0100

    new changes

--- src/app/grant/page.jsx
+++ src/app/grant/page.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Grant = () => {
   return (
-    
+    <section className="">
+      <div className="">
+       <video src="">
+        so
+       </video>
+      </div>
+    </section>
     
   )
 }
